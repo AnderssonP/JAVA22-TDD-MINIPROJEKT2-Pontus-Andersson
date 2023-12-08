26cec47b68c47c5c245a82634e149e0271853c99
Pontus Andersson <102531282+AnderssonP@users.noreply.github.com>
2023-12-08 09:00:21 +0100

    Update PrimeTest.java

--- src/test/PrimeTest.java
+++ src/test/PrimeTest.java
@@ -1,41 +1,38 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import main.Prime;
 
-public class PrimeTest {
-	
-	@Test
-	void calculateTest() {
-		
-		Prime prime = new Prime(1, 1000);
+class PrimeTest {
+
+    @Test
+    @DisplayName("Testa primtalsberäkning och listan")
+    void calculateTest() {
+        Prime prime = new Prime(1, 1000);
 
         List<Integer> primes = prime.getPrimes();
 
         assertNotNull(primes);
-
         assertFalse(primes.isEmpty());
 
         for (Integer primeNumber : primes) {
             assertNotNull(primeNumber);
         }
-		
-	}
-	
-	@Test
-	void isPrimeTest() {
-		Prime prime = new Prime(1, 1000);
-        
+    }
+
+    @Test
+    @DisplayName("Testa primtalen")
+    void isPrimeTest() {
+        Prime prime = new Prime(1, 1000);
+
         assertTrue(prime.numIsPrime(2));
         assertTrue(prime.numIsPrime(3));
         assertTrue(prime.numIsPrime(53));
@@ -46,10 +43,11 @@ public class PrimeTest {
         assertFalse(prime.numIsPrime(50));
         assertFalse(prime.numIsPrime(315));
         assertFalse(prime.numIsPrime(702));
-	}
-	
-	@Test
-    public void testInvalidIntervalMessage() {
+    }
+
+    @Test
+    @DisplayName("Testa meddelandet för ogiltigt intervall")
+    void testInvalidIntervalMessage() {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
@@ -65,9 +63,10 @@ public class PrimeTest {
 
         System.setOut(System.out);
     }
-	
-	@Test
-    public void testValidInterval() {
+
+    @Test
+    @DisplayName("Testa giltiga och ogiltiga intervall")
+    void testValidInterval() {
         assertTrue(Prime.isValidInterval(0, 1000));
         assertTrue(Prime.isValidInterval(500, 700));
         assertFalse(Prime.isValidInterval(1001, 1500));
@@ -75,4 +74,22 @@ public class PrimeTest {
         assertFalse(Prime.isValidInterval(-10, 100));
     }
 
+    @Test
+    @DisplayName("Testa null i primtalsberäkning")
+    void testNullValues() {
+        Prime prime = new Prime(0, 0);
+
+        List<Integer> primes = prime.getPrimes();
+
+        assertNotNull(primes);
+        assertTrue(primes.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Test för ogiltig primtalsberäkning")
+    void testInvalidCalculation() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Prime(1000, 0); 
+        });
+    }
 }
